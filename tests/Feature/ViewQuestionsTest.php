b6f09d5b64f231174e812ce39dee863a8cf23e42
yonh lai <azssjli@163.com>
2020-12-25 01:21:07 +0800

    add question model and factory

--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Question;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -18,6 +19,23 @@ class ViewQuestionsTest extends TestCase
 
         // 3. 正常返回 200
         $test->assertStatus(200);
+    }
 
+    /** @test */
+    public function user_can_view_a_single_question()
+    {
+        $this->withoutExceptionHandling();
+
+        // 1. 创建问题
+        $question = factory(Question::class)->create();
+
+        // 2. 访问链接
+        $test = $this->get('/questions/' . $question->id);
+
+
+        // 3. 应该能看到什么内容
+        $test->assertStatus(200)
+            ->assertSee($question->title)
+            ->assertSee($question->content);
     }
 }
