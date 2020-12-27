fd36811ae7944f289f6fa5a15be9e193abbe8fc3
yonh lai <azssjli@163.com>
2020-12-28 00:32:37 +0800

    用户不能回复为发布的问题

--- tests/Feature/PostAnswersTest.php
+++ tests/Feature/PostAnswersTest.php
@@ -13,10 +13,10 @@ class PostAnswersTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function user_can_post_an_answer_to_a_question()
+    public function user_can_post_an_answer_to_a_published_question()
     {
         // 假设已存在某个问题
-        $question = factory(Question::class)->create();
+        $question = factory(Question::class)->state('published')->create();
         $user = factory(User::class)->create();
 
         $response = $this->post("/questions/{$question->id}/answers",[
@@ -30,6 +30,23 @@ class PostAnswersTest extends TestCase
         $this->assertNotNull($answer);
 
         $this->assertEquals(1, $question->answers()->count());
+    }
 
+    /** @test */
+    public function can_not_post_an_answer_to_an_unpublished_question()
+    {
+        $question = factory(Question::class)->state('unpublished')->create();
+        $user = factory(User::class)->create();
+
+        $response = $this->withExceptionHandling()
+            ->post("/questions/{$question->id}/answers", [
+               'user_id'=> $user->id,
+               'content'=> 'this is an answer',
+            ]);
+
+        $response->assertStatus(404);
+
+        $this->assertDatabaseMissing('answers', ['question_id'=>$question->id]);
+        $this->assertEquals(0, $question->answers()->count());
     }
 }
